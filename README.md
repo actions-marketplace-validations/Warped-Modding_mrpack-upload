8030193f3d7d654915f53ef26eee78783c137013
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2022-11-22 20:53:53 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# mrpack-publish
+
+Publish .mrpacks to Modrinth.
+
+## Use this action
+
+```yaml
+- name: Publish .mrpack
+  uses: RyanModDev/mrpack-publish@v0
+```
---
+++ index.ts
@@ -0,0 +1,58 @@
+import { fetch } from 'ofetch';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+
+import { getInput, error } from '@actions/core';
+import { bold, green } from 'kleur/colors';
+
+const success = (a: string) => {
+  console.log(bold(green('✔')) + ' ' + a);
+};
+
+(async () => {
+  const inputs = {
+    NAME: getInput('name', { required: true }),
+    VERSION_NUMBER: getInput('version-number', { required: true }),
+    PROJECT_ID: getInput('project-id', { required: true }),
+    PACK_FILENAME: getInput('pack-filename', { required: true }),
+    GAME_VERSIONS: getInput('game-versions', { required: true }),
+    LOADERS: getInput('loaders', { required: true }),
+    MODRINTH_TOKEN: getInput('modrinth-token', { required: true }),
+  };
+
+  const f = await readFile(join(process.cwd(), inputs.PACK_FILENAME));
+  const file = new File([f], inputs.PACK_FILENAME);
+
+  const form = new FormData();
+
+  const data = {
+    name: inputs.NAME,
+    version_number: inputs.VERSION_NUMBER,
+    game_versions: inputs.GAME_VERSIONS.split(','),
+    version_type: 'release',
+    loaders: inputs.LOADERS.split(','),
+    featured: false,
+    project_id: inputs.PROJECT_ID,
+    file_parts: [inputs.PACK_FILENAME],
+    dependencies: [],
+  };
+  form.append('data', JSON.stringify(data));
+  form.append(inputs.PACK_FILENAME, file);
+
+  const res = await fetch('https://api.modrinth.com/v2/version', {
+    method: 'POST',
+    body: form,
+    headers: {
+      Authorization: inputs.MODRINTH_TOKEN,
+    },
+  });
+
+  if (!res.ok) {
+    error(await res.text());
+  }
+
+  success('Uploaded to Modrinth!');
+})().catch((e) => {
+  error(e);
+  process.exit(1);
+});
